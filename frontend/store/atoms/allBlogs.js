b586e133a2b2d502ba48f0383cc27616ea8fc203
Nikhil Raj <81584740+nikhil7759@users.noreply.github.com>
2024-09-28 14:07:32 +0530

    Add files via upload

---
+++ frontend/store/atoms/allBlogs.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { atom, selector } from "recoil";
+
+export const allBlogs = atom({
+  key: "allBlogs", // unique ID (with respect to other atoms/selectors)
+  default: selector({
+    key: "allBlogs/default",
+    get: async () => {
+      try {
+        const response = await axios.get(
+          `https://backend.sevenburgers.workers.dev/api/v1/blog/bulk`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        return { success: false, message: "error while fetching blogs" };
+      }
+    },
+  }),
+});
